f6f4de431ac3dc79a4c92b57344314e8beb1f0e1
sam <sam1424061933155@gmail.com>
2017-06-18 22:07:30 +0800

    6/18

--- comic/comic/final_v1.cpp
+++ comic/comic/final_v1.cpp
@@ -11,12 +11,22 @@
 #include <opencv2/opencv.hpp>
 #include <opencv/cvaux.hpp>
 #include <opencv2/video/video.hpp>
-#include "cartoon.h"
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/image_io.h>
+#include <dlib/opencv/cv_image_abstract.h>
+
 
 using namespace std;
 using namespace cv;
+
+struct correspondens{
+    vector<int> index;
+};
 
 /** Global variables */
 String face_cascade_name = "/Users/sam/Desktop/cv_env/opencv/data/haarcascades/haarcascade_frontalface_alt.xml";
@@ -421,6 +431,195 @@ void cartoon(Mat &Frame,Mat &output){
     
     
 }
+void faceLandmarkDetection(dlib::array2d<unsigned char>& img, dlib::shape_predictor sp, std::vector<Point2f>& landmark)
+{
+    dlib::frontal_face_detector detector = dlib::get_frontal_face_detector();
+    
+    std::vector<dlib::rectangle> dets = detector(img);
+    
+    
+    dlib::full_object_detection shape = sp(img, dets[0]);
+    
+    for (int i = 0; i < shape.num_parts(); ++i)
+    {
+        float x=shape.part(i).x();
+        float y=shape.part(i).y();
+        landmark.push_back(Point2f(x,y));		
+    }
+    
+    
+}
+void delaunayTriangulation(const std::vector<Point2f>& hull,std::vector<correspondens>& delaunayTri,Rect rect)
+{
+    
+    cv::Subdiv2D subdiv(rect);
+    for (int it = 0; it < hull.size(); it++)
+        subdiv.insert(hull[it]);
+    //cout<<"done subdiv add......"<<endl;
+    std::vector<Vec6f> triangleList;
+    subdiv.getTriangleList(triangleList);
+   
+    for (size_t i = 0; i < triangleList.size(); ++i)
+    {
+        
+        std::vector<Point2f> pt;
+        correspondens ind;
+        Vec6f t = triangleList[i];
+        pt.push_back( Point2f(t[0], t[1]) );
+        pt.push_back( Point2f(t[2], t[3]) );
+        pt.push_back( Point2f(t[4], t[5]) );
+        
+        if (rect.contains(pt[0]) && rect.contains(pt[1]) && rect.contains(pt[2]))
+        {
+            int count = 0;
+            for (int j = 0; j < 3; ++j)
+                for (size_t k = 0; k < hull.size(); k++)
+                    if (abs(pt[j].x - hull[k].x) < 1.0   &&  abs(pt[j].y - hull[k].y) < 1.0)
+                    {
+                        ind.index.push_back(k);
+                        count++;
+                    }
+            if (count == 3)
+                delaunayTri.push_back(ind);
+        }
+       
+    }	
+    
+    
+}
+void applyAffineTransform(Mat &warpImage, Mat &src, std::vector<Point2f> &srcTri, std::vector<Point2f> &dstTri)
+{
+    // Given a pair of triangles, find the affine transform.
+    Mat warpMat = getAffineTransform( srcTri, dstTri );
+    
+    // Apply the Affine Transform just found to the src image
+    warpAffine( src, warpImage, warpMat, warpImage.size(), cv::INTER_LINEAR, BORDER_REFLECT_101);
+}
+
+void warpTriangle(Mat &img1, Mat &img2, std::vector<Point2f> &t1, std::vector<Point2f> &t2)
+{
+    
+    Rect r1 = boundingRect(t1);
+    Rect r2 = boundingRect(t2);
+    
+    // Offset points by left top corner of the respective rectangles
+    std::vector<Point2f> t1Rect, t2Rect;
+    std::vector<Point> t2RectInt;
+    for(int i = 0; i < 3; i++)
+    {
+        
+        t1Rect.push_back( Point2f( t1[i].x - r1.x, t1[i].y -  r1.y) );
+        t2Rect.push_back( Point2f( t2[i].x - r2.x, t2[i].y - r2.y) );
+        t2RectInt.push_back( Point(t2[i].x - r2.x, t2[i].y - r2.y) ); // for fillConvexPoly
+        
+    }
+    
+    // Get mask by filling triangle
+    Mat mask = Mat::zeros(r2.height, r2.width, CV_32FC3);
+    fillConvexPoly(mask, t2RectInt, Scalar(1.0, 1.0, 1.0), 16, 0);
+    
+    Mat img1Rect;
+    img1(r1).copyTo(img1Rect);
+    
+    Mat img2Rect = Mat::zeros(r2.height, r2.width, img1Rect.type());
+    
+    applyAffineTransform(img2Rect, img1Rect, t1Rect, t2Rect);
+    
+    multiply(img2Rect,mask, img2Rect);
+    multiply(img2(r2), Scalar(1.0,1.0,1.0) - mask, img2(r2));
+    img2(r2) = img2(r2) + img2Rect;
+    
+}
+
+
+
+void faceSwap(Mat &Frame){
+    
+    //----------------- step 1. load the input two images. ----------------------------------
+    dlib::array2d<unsigned char> imgDlib1,imgDlib2;
+    imwrite("/Users/sam/Desktop/course/cv/final_project/frame.png",Frame);
+    
+    dlib::load_image(imgDlib1,"/Users/sam/Desktop/course/cv/final_project/obama.png");
+    dlib::load_image(imgDlib2,"/Users/sam/Desktop/course/cv/final_project/frame.png" );
+    
+    Mat imgCV1 = imread("/Users/sam/Desktop/course/cv/final_project/obama.png");
+    Mat imgCV2 = imread("/Users/sam/Desktop/course/cv/final_project/frame.png");
+    if(!imgCV1.data || !imgCV2.data)
+    {
+        printf("No image data \n");
+    }
+    else
+        cout<<"image readed by opencv"<<endl;
+    //---------------------- step 2. detect face landmarks -----------------------------------
+    dlib::shape_predictor sp;
+    dlib::deserialize("/Users/sam/Desktop/course/cv/final_project/shape_predictor_68_face_landmarks.dat") >> sp;
+    std::vector<Point2f> points1, points2;
+    
+    faceLandmarkDetection(imgDlib1,sp,points1);
+    faceLandmarkDetection(imgDlib2,sp,points2);
+    
+    //---------------------step 3. find convex hull -------------------------------------------
+    Mat imgCV1Warped = imgCV2.clone();
+    imgCV1.convertTo(imgCV1, CV_32F);
+    imgCV1Warped.convertTo(imgCV1Warped, CV_32F);
+    
+    std::vector<Point2f> hull1;
+    std::vector<Point2f> hull2;
+    std::vector<int> hullIndex;
+    
+    cv::convexHull(points2, hullIndex, false, false);
+    
+    for(int i = 0; i < hullIndex.size(); i++)
+    {
+        hull1.push_back(points1[hullIndex[i]]);
+        hull2.push_back(points2[hullIndex[i]]);
+    }
+    //-----------------------step 4. delaunay triangulation -------------------------------------
+    std::vector<correspondens> delaunayTri;
+    Rect rect(0, 0, imgCV1Warped.cols, imgCV1Warped.rows);
+    delaunayTriangulation(hull2,delaunayTri,rect);
+    
+    for(size_t i=0;i<delaunayTri.size();++i)
+    {
+        std::vector<Point2f> t1,t2;
+        correspondens corpd=delaunayTri[i];
+        for(size_t j=0;j<3;++j)
+        {
+            t1.push_back(hull1[corpd.index[j]]);
+            t2.push_back(hull2[corpd.index[j]]);
+        }
+        
+        warpTriangle(imgCV1,imgCV1Warped,t1,t2);			
+    }
+    //------------------------step 5. clone seamlessly -----------------------------------------------
+    
+    //calculate mask
+    std::vector<Point> hull8U;
+    
+    for(int i=0; i< hull2.size();++i)
+    {
+        Point pt(hull2[i].x,hull2[i].y);
+        hull8U.push_back(pt);
+    }
+    
+    
+    Mat mask = Mat::zeros(imgCV2.rows,imgCV2.cols,imgCV2.depth());
+    fillConvexPoly(mask, &hull8U[0], hull8U.size(), Scalar(255,255,255));
+    
+    
+    Rect r = boundingRect(hull2);
+    Point center = (r.tl() +r.br()) / 2;
+    
+    Mat output;
+    imgCV1Warped.convertTo(imgCV1Warped, CV_8UC3);
+    seamlessClone(imgCV1Warped,imgCV2,mask,center,output,NORMAL_CLONE);
+    
+    imshow("result",output);
+
+    
+    
+
+}
 
 
 
@@ -456,6 +655,7 @@ int main(int argc, const char * argv[]) {
     nose_cascade.load(nose_cascade_name);
     
     int type=1;
+    int count = 0;
 
     
     while (1)
@@ -496,16 +696,28 @@ int main(int argc, const char * argv[]) {
             cout <<"5 is click "<<endl;
             type=5;
             
+        }else if(keypress == 54){
+            cout <<"6 is click "<<endl;
+            type=6;
+            
         }
-        if(type!=4 && type!=5){
+        if(type!=4 && type!=5 && type!=6){
             cartoonTransform(Frame, output,type);
+            count = 0;
 
         }else if(type==4){
             cartoon(Frame, output);
-        }else{
+            count = 0;
+        }else if(type==5){
             sketch(Frame, output);
+            count = 0;
+        }else if(type==6){
+            count++;
+            output = Frame;
+            if(count==1){
+                faceSwap(Frame);
+            }
         }
-        //sketch(Frame,output);
         // 使用图像处理技术将获取的帧经过处理后输入到output中
         namedWindow("Cartoon", CV_WINDOW_NORMAL);
         imshow("Cartoon", output);
